751a04006cb99b06456b167c55ce57aea96f4924
arunkumarsa7 <arunkumarsa8374@gmail.com>
2019-11-26 11:30:45 +0530

    Upgraded the build tools.

--- Deployment/MySQL/MySQLScriptRunner.bat
+++ Deployment/MySQL/MySQLScriptRunner.bat
@@ -1,20 +1,42 @@
 @echo off
-echo            ----------------------
-echo     	  -------made by @run-----
-echo            ----------------------
-echo  _ ----------------------------------------------- _
-echo -_           MySQL Script Runner                   _ -
-echo    -----------------------------------------------
+echo     __  ___      _____ ____    __       _____           _       __     ____                             
+echo    /  //  /_  __/ ___// __ \  / /      / ___/__________(_)___  / /_   / __ \__  ______  ____  ___  _____
+echo   / //_/ / / / /\__ \/ / / / / /       \__ \/ ___/ ___/ / __ \/ __/  / /_/ / / / / __ \/ __ \/ _ \/ ___/
+echo  / /  / / /_/ /___/ / /_/ / / /___    ___/ / /__/ /  / / /_/ / /_   / _, _/ /_/ / / / / / / /  __/ /    
+echo /_/  /_/\__, //____/\___\_\/_____/   /____/\___/_/  /_/ .___/\__/  /_/ /_/\__,_/_/ /_/_/ /_/\___/_/     
+echo        /____/                                        /_/                                                
 echo.
-set MY_SQL_ROOT=C:\Program Files\MySQL\MySQL Server 8.0\bin
-set MY_SQL_USER_NAME=root
-set MY_SQL_PASSWORD=root
-set SCRIPT_FILES_ROOT=C:\Users\Arun Kumar S A\Documents\PortfolioManagerDocs\SQL Scripts
-set SCRIPT_LOG_ROOT=C:\Users\Public\PortfolioManager\logs
+
+set PROPERTY_FILE=%cd%\config\SQL.properties
+set ROOT_LOCATION=%cd%
+
+set File=%PROPERTY_FILE%
+set /a count=0
+SETLOCAL enabledelayedexpansion
+for /F "tokens=* delims=" %%a in ('Type "%File%"') do (
+    Set /a count+=1
+    Set "output[!count!]=%%a"     
+)
+
+For /L %%i in (1,1,%Count%) Do (	
+	for /f "tokens=1,2 delims==" %%a in ("!output[%%i]!") do (
+		IF "%%a"=="MY_SQL_ROOT" (
+			set MY_SQL_ROOT=%%b
+		)
+		IF "%%a"=="SCRIPT_FILES_ROOT" (
+			set SCRIPT_FILES_ROOT=%%b
+		)
+	)
+)
 
 cd /d %MY_SQL_ROOT%
-mysql --user=%MY_SQL_USER_NAME% --password=%MY_SQL_PASSWORD% PORTFOLIO_MANAGER < "%SCRIPT_FILES_ROOT%\1_PORTFOLIO_MANAGER_DDL_DROP_SCRIPTS.sql"
-mysql --user=%MY_SQL_USER_NAME% --password=%MY_SQL_PASSWORD% PORTFOLIO_MANAGER < "%SCRIPT_FILES_ROOT%\2_PORTFOLIO_MANAGER_DDL_CREATE_SCRIPTS.sql"
-mysql --user=%MY_SQL_USER_NAME% --password=%MY_SQL_PASSWORD% PORTFOLIO_MANAGER < "%SCRIPT_FILES_ROOT%\3_PORTFOLIO_MANAGER_DDL_ALTER_SCRIPTS.sql"
-mysql --user=%MY_SQL_USER_NAME% --password=%MY_SQL_PASSWORD% PORTFOLIO_MANAGER < "%SCRIPT_FILES_ROOT%\4_PORTFOLIO_MANAGER_DDL_INDEX_SCRIPTS.sql"
-mysql --user=%MY_SQL_USER_NAME% --password=%MY_SQL_PASSWORD% PORTFOLIO_MANAGER < "%SCRIPT_FILES_ROOT%\5_PORTFOLIO_MANAGER_DML_INSERT_SCRIPTS.sql"
+
+setlocal enabledelayedexpansion
+set /a count = 1
+for %%f in ("%SCRIPT_FILES_ROOT%\*.sql") do (  
+  echo Running...  
+  echo !count!. "%%f"
+  mysql --defaults-extra-file="%ROOT_LOCATION%\config\config.cnf" PORTFOLIO_MANAGER < %%f 
+  set /a count += 1
+  echo.
+)
